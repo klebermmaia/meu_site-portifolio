f3dde9de7723a731d7582e7704d1e4550c3b568c
klebermmaia <klebermmaia@gmail.com>
2022-11-12 00:48:23 -0300

    campo de habilidades

--- js/modules/slide-details.js
+++ js/modules/slide-details.js
@@ -3,18 +3,17 @@ export default async function initSlidedDetails() {
   const dadosProjetos = await fetch('../../projetos.json')
   const projetosJSON = await dadosProjetos.json();
   
-  for(let i = 0; i< projetosJSON.length; i++){
+  for (let i = 0; i < projetosJSON.length; i++) {
     const newLi = document.createElement('li');
     const newImg = document.createElement('img');
 
-    ['item', 'swiper-slide'].forEach(className =>{
+    ['item', 'swiper-slide'].forEach(className => {
       newLi.classList.add(className);
     });
     newLi.style.backgroundColor = projetosJSON[i].cor;
-    
-    ['src','alt'].forEach(att=>{
+
+    ['src', 'alt'].forEach(att => {
       newImg.setAttribute(att, projetosJSON[i][att]);
-      console.log(newImg)
     });
 
     newLi.appendChild(newImg);
@@ -25,4 +24,15 @@ export default async function initSlidedDetails() {
     </span>`
     boxPrejetos.appendChild(newLi);
   }
+
+  const itens = document.querySelectorAll('.item');
+  const usedTech = document.querySelector('.used-tech');
+  setInterval(() => {
+    itens.forEach(i=>{
+      if(i.classList.contains('swiper-slide-active')){
+        console.log('ok')
+        usedTech.style.backgroundColor = '#ffffff'
+      }
+    })
+  }, 1000);
 }
